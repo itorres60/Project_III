fe6891d16447ce0d2d7a2daceea58c8d99775df7
J <torres.j.isaias@gmail.com>
2021-11-20 09:11:53 -0600

    added burger menu with minimum styling

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { slide as Menu } from 'react-burger-menu'
+import styles from './burger_styles.css'
 
 import Auth from '../../utils/auth';
 
@@ -9,14 +11,39 @@ const Header = () => {
     Auth.logout();
   };
 
+  function showSettings (event) {
+    event.preventDefault();
+  }  
+
   return (
-    <header className="bg-secondary mb-4 py-2 flex-row align-center">
+    <header styles={styles} className="bg-secondary mb-4 py-2 flex-row align-center">
+
+      <Menu>
+       {Auth.loggedIn() ? (
+          <>
+            <Link to="/profile">Me</Link>
+            <a href="/" onClick={logout}>
+              Logout
+            </a>
+          </>
+        ) : (
+          <>
+            <Link to="/login">Login</Link>
+            <Link to="/signup">Signup</Link>
+          </>
+        )}
+        {/* <a id="home" className="menu-item" href="/">Home</a>
+        <a id="about" className="menu-item" href="/about">About</a>
+        <a id="contact" className="menu-item" href="/contact">Contact</a>
+        <a onClick={ showSettings } className="menu-item--small" href="">Settings</a> */}
+      </Menu>
+
       <div className="container flex-row justify-space-between-lg justify-center align-center">
         <Link to="/">
           <h1>Project III</h1>
         </Link>
 
-        <nav className="text-center">
+        <nav className="text-center nav-bar">
           {Auth.loggedIn() ? (
             <>
               <Link to="/profile">Me</Link>
