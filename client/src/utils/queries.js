f65f616426a94beedded2de1c644002ff234a700
Zach Yarbrough <zachyarbro@gmail.com>
2021-11-24 20:09:11 -0600

    add client queries and mutation

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -11,14 +11,53 @@ export const QUERY_USER = gql`
   }
 `;
 
+export const QUERY_USERS = gql`
+    query users {
+      users {
+        _id
+        firstName
+        lastName
+        email
+        calendars {
+          _id
+        }
+        role
+      }
+    }
+`
+
 export const QUERY_ME = gql`
-  {
-    me {
+    query me {
       _id
       firstName
       lastName
       email
       role
+    }
+`;
+
+export const QUERY_CALENDARS = gql`
+    query calendars {
+      calendars {
+        _id
+        companyName
+        users
+        admin {
+          _id
+        }
+      }
+    }
+`;
+
+export const QUERY_CALENDAR = gql`
+  query calendar($calendarId: ID!) {
+    calendar(calendarId: $calendarId) {
+      _id
+      companyName
+      users
+      admin {
+        _id
+      }
     }
   }
 `;
