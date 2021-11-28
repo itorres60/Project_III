d45620d3d0efee2ada82d8ca73835398cec7f141
Bailey Poe <baileypoe1222@gmail.com>
2021-11-28 17:51:25 -0600

    started modals for creating and deleting calendars

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -6,6 +6,7 @@ import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 
+
 const Profile = () => {
   const createCalendar = () => {
     console.log('Creating new calendar...')
@@ -21,16 +22,7 @@ const Profile = () => {
   if(data.me.role === 'administrator') {
     // needs an input allowing the admin to create a calendar
     return (
-      <Card sx={{ minWidth: 275 }} style={{backgroundColor:'#333'}}>
-      <CardContent>
-      <div style={{color:'#fff'}}>
-      <CardActions className='flex-column'>
-        <Button size="large" variant="contained" style={{ backgroundColor: '#9C27B0', marginBottom: '1rem'}} onClick={createCalendar}><span style={{fontSize:'1.5rem', marginRight: '0.5rem'}}>+</span> Create Calendar</Button>
-        <Button size="large" variant="contained" onClick={deleteCalendar}><span style={{fontSize:'1.15rem', marginRight: '0.5rem'}}>X</span> Delete Calendar</Button>
-        </CardActions>
-      </div>
-      </CardContent>
-      </Card>
+      <div id='modalTarget'></div>
     );
   } else if (data.me.role === 'employee' || data.me.role === 'reliever') {
     return (
