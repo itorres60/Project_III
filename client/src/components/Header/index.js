01a7fd59564191d3a9779b25cc2f127ddbd58e8a
Bailey Poe <baileypoe1222@gmail.com>
2021-12-01 19:33:23 -0600

    last second additions

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -15,7 +15,7 @@ const Header = () => {
   };
 
   return (
-    <header  className="bg-tertiary mb-4 py-2 flex-row align-center">
+    <header className="bg-tertiary mb-4 py-2 flex-row align-center">
 
 
       <div className="container flex-row justify-space-between-lg justify-center align-center">
